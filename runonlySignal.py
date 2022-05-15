9eee3a32198d540ccd6924055bc5cc6e062869c3
shravan9912 <shravan9912@gmail.com>
2022-05-15 22:57:59 +0530

    Update runonlySignal.py

--- runonlySignal.py
+++ runonlySignal.py
@@ -19,7 +19,7 @@ def signalonly():
     pdfkit.from_url('http://192.168.1.1/index.html',"signaltestonly/"+ststr+"start.pdf")
     file_size = os.path.getsize("signaltestonly/"+ststr+".pdf")
     int(file_size)
-    while file_size < 20048:
+    while file_size < 400480:
        pdfkit.from_url('http://192.168.1.1/index.html',"signaltestonly/"+ststr+".pdf")
        file_size = os.path.getsize("signaltestonly/"+ststr+".pdf")
     et=current_time=datetime.datetime.now()
